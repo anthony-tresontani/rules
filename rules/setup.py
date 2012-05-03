cf9a85ca8fd93b79672085a8d311f12cfe362627
Anthony Tresontani <tresontani@gmail.com>
2012-05-03 12:17:49 +0100

    Add a setup.py installation script

---
+++ rules/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='django-rules',
+      version='0.1',
+      description='Dynamic rule handling',
+      author='Anthony Tresontani',
+      author_email='dev.tresontani@gmail.com',
+      packages=['core'],
+     )
