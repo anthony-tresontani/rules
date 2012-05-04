67f9daa5c2bd1ae835414379ec6b32a41a5aecc3
Anthony Tresontani <tresontani@gmail.com>
2012-05-04 11:21:26 +0100

    Change int he setup.py from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='django-rules',
       version='0.1',
       description='Dynamic rule handling',
       author='Anthony Tresontani',
       author_email='dev.tresontani@gmail.com',
-      packages=['rules_engine'],
+      packages=[find_package('rules_engine')],
       install_requires = ['PEAK-Rules==0.5a1.dev-r2707']
      )
