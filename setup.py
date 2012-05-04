1b07960376c70dfd20d725c2248d6ecf8649e0b5
Anthony Tresontani <tresontani@gmail.com>
2012-05-04 11:26:59 +0100

    Still fixing setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='django-rules',
       version='0.1',
       description='Dynamic rule handling',
       author='Anthony Tresontani',
       author_email='dev.tresontani@gmail.com',
-      packages=[find_packages('rules_engine')],
+      packages=['rules_engine', 'rules_engine.rules_handler'],
       install_requires = ['PEAK-Rules==0.5a1.dev-r2707']
      )
