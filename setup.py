d6c4ccfd15dc45e23bf1885445a69e0ea809f5b8
Anthony Tresontani <tresontani@gmail.com>
2012-07-12 10:59:33 +0200

    Bundle release 0.2.0

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='django-rules',
-      version='0.1',
+      version='0.2.0',
       description='Dynamic rule handling',
       author='Anthony Tresontani',
       author_email='dev.tresontani@gmail.com',
       packages=['rules'],
-      install_requires = ['PEAK-Rules==0.5a1.dev-r2707']
+      install_requires = ['PEAK-Rules==0.5a1.dev-r2707', 'django==1.2']
      )
